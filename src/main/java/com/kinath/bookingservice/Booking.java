c9254e11a2bcddc715d802a8c51c90c6b80ddaa6
Kinath Rupasinghe <kinath.ru@gmail.com>
2017-02-25 11:27:48 +0530

    Adding source files

---
+++ src/main/java/com/kinath/bookingservice/Booking.java
@@ -0,0 +1,12 @@
+package com.kinath.bookingservice;
+
+/**
+ * Created by Kinath on 2/25/2017.
+ */
+public class Booking {
+
+    private long bookingId;
+    private String bookingType;
+
+
+}
---
+++ src/main/java/com/kinath/bookingservice/BookingService.java
@@ -0,0 +1,7 @@
+package com.kinath.bookingservice;
+
+/**
+ * Created by Kinath on 2/25/2017.
+ */
+public class BookingService {
+}
