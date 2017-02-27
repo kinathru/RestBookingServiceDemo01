27cde513d44eacbe7ea751f6dca87ec1c720cf71
Kinath Rupasinghe <kinath.ru@gmail.com>
2017-02-27 23:29:43 +0530

    implement services and add web xmls

--- src/main/java/com/kinath/bookingservice/BookingService.java
+++ src/main/java/com/kinath/bookingservice/BookingService.java
@@ -1,6 +1,10 @@
 package com.kinath.bookingservice;
 
 import com.kinath.bookingservice.util.BookingLoaderUtil;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -8,21 +12,23 @@ import java.util.List;
 /**
  * Created by Kinath on 2/25/2017.
  */
-@RestController
-public class BookingService
+@RestController public class BookingService
 {
-    public Booking loadBooking(long bookingId)
+    @RequestMapping(value = "/booking", method = RequestMethod.GET)
+    public Booking loadBooking( @RequestParam(value = "bookingId", required = true) long bookingId )
     {
         return BookingLoaderUtil.loadBooking( bookingId );
     }
 
+    @RequestMapping(value = "/bookings", method = RequestMethod.GET)
     public List<Booking> loadAllBookings()
     {
         return BookingLoaderUtil.loadBookingsList();
     }
 
-    public String addBooking(Booking booking)
+    @RequestMapping(value = "/addbooking", method = RequestMethod.POST)
+    public String addBooking( @RequestBody Booking booking )
     {
-        return BookingLoaderUtil.addBooking(booking);
+        return BookingLoaderUtil.addBooking( booking );
     }
 }
