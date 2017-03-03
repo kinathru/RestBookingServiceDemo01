b78f60f5ac2efe468d2285078bc9966108173aab
Kinath Rupasinghe <kinath.ru@gmail.com>
2017-03-03 23:15:31 +0530

    adding booking search functionality

--- src/main/java/com/kinath/bookingservice/BookingService.java
+++ src/main/java/com/kinath/bookingservice/BookingService.java
@@ -1,6 +1,8 @@
 package com.kinath.bookingservice;
 
 import com.kinath.bookingservice.util.BookingLoaderUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -40,5 +44,14 @@ public class BookingService
     public String addBooking( @RequestBody Booking booking )
     {
         return BookingLoaderUtil.addBooking( booking );
+    }
+
+    public ResponseEntity<List<Booking>> searchBookings( HttpServletRequest httpServletRequest)
+    {
+        List<Booking> filteredBookings = new ArrayList<Booking>();
+
+        //TODO: Implement booking search method
+
+        return new ResponseEntity<List<Booking>>( filteredBookings, HttpStatus.OK );
     }
 }
